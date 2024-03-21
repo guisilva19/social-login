c2eb1801d16b8af69e6e242ebcc7a866b5e08539
Guilherme Silva <2003silvagui@gmail.com>
2024-03-21 01:49:58 -0300

    feat: ajusts

--- src/screens/login/page.tsx
+++ src/screens/login/page.tsx
@@ -7,6 +7,13 @@ export default function Login() {
       <div>
         <h1>Login</h1>
         <GoogleLogin
+          width={50}
+          theme="outline"
+          shape="circle"
+          text="signin_with"
+          logo_alignment="center"
+          ux_mode="popup"
+          locale="pt"
           onSuccess={(response) => {
             const infoUser = jwtDecode(response.credential);
             console.log(infoUser);
