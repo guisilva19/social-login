655925fa5ab3784a180cdd925decdb17efa32959
Guilherme Silva <2003silvagui@gmail.com>
2024-03-22 01:31:44 -0300

    feat: form login

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/routes";
 import { GOOGLE_CLIENT_ID } from "./config/enviroments";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- src/screens/login/page.tsx
+++ src/screens/login/page.tsx
@@ -1,25 +1,43 @@
 import { GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
+import {
+  Container,
+  LoginForm,
+  Input,
+  LoginButton,
+  SocialButtons,
+  LoginInputs,
+  TitleForm,
+} from "./styled";
 
 export default function Login() {
   return (
     <>
-      <div>
-        <h1>Login</h1>
-        <GoogleLogin
-          width={50}
-          theme="outline"
-          shape="circle"
-          text="signin_with"
-          logo_alignment="center"
-          ux_mode="popup"
-          locale="pt"
-          onSuccess={(response) => {
-            const infoUser = jwtDecode(response.credential);
-            console.log(infoUser);
-          }}
-        />
-      </div>
+      <Container>
+        <LoginForm>
+          <TitleForm>Login</TitleForm>
+          <LoginInputs>
+            <Input type="email" placeholder="Email" />
+            <Input type="password" placeholder="Password" />
+          </LoginInputs>
+          <LoginButton>Entrar</LoginButton>
+          <SocialButtons>
+            <GoogleLogin
+              width={50}
+              theme="outline"
+              shape="circle"
+              text="signin_with"
+              logo_alignment="center"
+              ux_mode="popup"
+              locale="pt"
+              onSuccess={(response) => {
+                const infoUser = jwtDecode(response.credential);
+                console.log(infoUser);
+              }}
+            />
+          </SocialButtons>
+        </LoginForm>
+      </Container>
     </>
   );
 }
