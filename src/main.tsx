5508905bbf43a8d69523b5f9545b63edac661ed1
Guilherme Silva <2003silvagui@gmail.com>
2024-04-11 14:17:29 -0300

    feat: correct

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/routes";
-import { GOOGLE_CLIENT_ID } from "./config/enviroments";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-      <RouterProvider router={router} />
-    </GoogleOAuthProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
